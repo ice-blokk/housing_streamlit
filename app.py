b0b38e68f7d070cb02559c7101ba5b3595bf9ce3
anthonytan <anthonythomastan@gmail.com>
2024-06-24 13:45:57 -0400

    updated intro text

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import numpy as np
 st.title('HousingMatch')
 st.info("""Hello there! We’re delighted you’re here! 
         Tell us what you’re looking for below, and see a curated list of 
-        properties that fit within your Emergency Housing Voucher (EHV) limit.  
-        At this time, we are only serving households with EHV vouchers in New York City.""")
+        properties that fit within your Emergency Housing Voucher (EHV) limit.""")
+st.info("At this time, we are only serving households with EHV vouchers in New York City.")
 
 st.header('Criteria')
 col1, col2, col3= st.columns(3)
